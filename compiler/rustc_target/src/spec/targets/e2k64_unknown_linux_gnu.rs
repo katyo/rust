e6510afe30231710aacd4c56641c9ccda823469f
K. <kayo@illumium.org>
2024-11-29 03:39:32 +0500

    e2k: Fix data layout

--- compiler/rustc_target/src/spec/targets/e2k64_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/targets/e2k64_unknown_linux_gnu.rs
@@ -25,7 +25,7 @@ pub fn target() -> Target {
         },
         pointer_width: 64,
         data_layout:
-            "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-f80:128:128-f128:128:128-v128:128:128-n32:64".into(),
+            "e-m:e-p:64:64-i64:64:64-f80:128:128-n32:64-S128".into(),
         arch: "e2k64".into(),
         options: base,
     }
